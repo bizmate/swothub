70df53d425aefba62d13a7400a8fe729a4903823
mhafellner <markus.hafellner@gmail.com>
2015-06-28 10:37:45 +0100

    checkout and confirmed

--- symfony/web/flight.js
+++ symfony/web/flight.js
@@ -1,18 +1,46 @@
 var app = angular.module('flightApp', [], function($locationProvider) {
   $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
+    enabled: true
   });
 });
 
 app.controller('flightController', function($scope, $http, $location) {
-  var from = $scope.from = $location.search()['from']
-  var to = $scope.to = $location.search()['to']
-  $http.get('http://localhost:8889/api/tos/'+to+'/froms/'+from+'/startdates/2015-07-10/enddates/2015-07-11.json').
+  var from = $location.search()['from']
+  var to = $location.search()['to']
+  var startdates = $scope.start = $location.search()['start']
+  var enddates = $scope.end = $location.search()['end']
+  $http.get('./app_dev.php/api/tos/'+to+'/froms/'+from+'/startdates/'+startdates+'/enddates/'+enddates+'.json').
     success(function(data, status, headers, config) {
       $scope.flights = data;
     }).
     error(function(data, status, headers, config) {
       // log error
     });
+    $http.get('./app_dev.php/api/citynames/'+from+'.json').
+      success(function(data, status, headers, config) {
+        $scope.from = data;
+      }).
+      error(function(data, status, headers, config) {
+        // log error
+      });
+      $http.get('./app_dev.php/api/citynames/'+to+'.json').
+        success(function(data, status, headers, config) {
+          $scope.to = data;
+            $scope.toImage = './images/destination/'+data+'.jpg';
+        }).
+        error(function(data, status, headers, config) {
+          // log error
+        });
+
+});
+
+app.directive('backImg', function(){
+  return function(scope, element, attrs){
+    attrs.$observe('backImg', function(value) {
+      element.css({
+        'background-image': 'url(' + value +')',
+        'background-size' : 'cover'
+      });
+    });
+  };
 });
--- symfony/web/maverick.js
+++ symfony/web/maverick.js
@@ -6,21 +6,50 @@ var app = angular.module('maverickApp', [], function($locationProvider) {
 
 app.controller('MaverickController', function($scope, $http, $location) {
   var budget = $location.search()['budget']
-  var startdates = $location.search()['start']
-  var enddates = $location.search()['end']
+  var startdates = $scope.start = $location.search()['start']
+  var enddates = $scope.end = $location.search()['end']
 
-  $http.get('api/tos/'+'FCO'+'/froms/LHR/startdates/'+startdates+'/enddates/'+enddates+'.json').
+  $http.get('app_dev.php/api/tos/'+'FCO'+'/froms/LHR/startdates/'+startdates+'/enddates/'+enddates+'.json').
     success(function(data, status, headers, config) {
       $scope.experiences = data['local_offers'];
       $scope.accommodations = data['hotels'];
       $scope.destinations = data['itineraries'];
-
+      var humanName;
+      $http.get('app_dev.php/api/citynames/'+'FCO'+'.json').
+        success(function(data, status, headers, config) {
+          $scope.destinations.forEach(function(entry) {
+            entry.name= data;
+          });
+        })
+        .error(function(data, status, headers, config) {});
       // maverick.destinations[0].price = data['itineraries'][0]['total']
     }).
     error(function(data, status, headers, config) {
       // log error
     });
+
+    $scope.changedDestination = function(code) {
+      $http.get('api/tos/'+code+'/froms/LHR/startdates/'+startdates+'/enddates/'+enddates+'.json').
+        success(function(data, status, headers, config) {
+          $scope.experiences = data['local_offers'];
+          $scope.accommodations = data['hotels'];
+          var humanName;
+          $http.get('app_dev.php/api/citynames/'+'FCO'+'.json').
+            success(function(data, status, headers, config) {
+              $scope.destinations.forEach(function(entry) {
+                entry.name= data;
+              });
+            })
+            .error(function(data, status, headers, config) {});
+          // maverick.destinations[0].price = data['itineraries'][0]['total']
+        }).
+        error(function(data, status, headers, config) {
+          // log error
+        });
+    };
   });
+
+
 
   app.directive('backImg', function(){
     return function(scope, element, attrs){
